f2d3e41d28aa861223c203b7e2093fa59318ca5a
Razvan <rabe@dhigroup.com>
2020-08-17 14:17:22 +0200

    fix(boilerplate): Add webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  externals: [
+    externalForMaterialUi,
+    {
+      'env-cmd': 'env-cmd',
+      formik: 'formik',
+      'formik-material-ui': 'formik-material-ui',
+      'prop-types': 'prop-types',
+      react: 'react',
+      'react-dom': 'react-dom',
+      'react-spinners': 'react-spinners',
+      yup: 'yup',
+    },
+  ],
+};
+
+function externalForMaterialUi(context, request, callback) {
+  if (/@material-ui.+/.test(request)) {
+    const name = request.replace(/^.*[\\\/]/, '');
+    return callback(null, 'root MaterialUI.' + name);
+  }
+  callback();
+}
