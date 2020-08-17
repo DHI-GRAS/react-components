3ed5a4e55c655f712a1504797c8c0e4be2289f34
Razvan <rabe@dhigroup.com>
2020-08-17 16:10:14 +0200

    fix(boilerplate): Change exports in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 export { default as Information } from './components/Information';
-export * from './components/Information';
 
 export { default as DynamicForm } from './components/DynamicForm';
-export * from './components/DynamicForm';
 
 export { default as SidebarTitle } from './components/Title';
-export * from './components/Title';
