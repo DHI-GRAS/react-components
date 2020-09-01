2d72e79d8aa1a561b2dc302d57c005435f6cdd58
Razvan <rabe@dhigroup.com>
2020-09-01 09:40:05 +0200

    feat(SidebarTitleTest): Add unit testing for SidebarTitle

--- src/components/SidebarTitle/SidebarTitle.test.tsx
+++ src/components/SidebarTitle/SidebarTitle.test.tsx
@@ -1,17 +1,61 @@
 import 'jsdom-global/register';
 import * as React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import SidebarTitle from './SidebarTitle';
 describe('SidebarTitle', () => {
-  it('has title', () => {
-    let wrap = mount<typeof SidebarTitle>(<SidebarTitle title="Its me" />);
-    expect(wrap.find('h2').text()).toEqual('Its me');
-  });
-
-  it('has subtitles', () => {
+  it('has title and color', () => {
     let wrap = mount<typeof SidebarTitle>(
-      <SidebarTitle title="Its me" subTitle={['subtitle1', 'subtitle2']} />
+      <SidebarTitle title="Its me" titleColor="#FFFFFF" />
+    );
+    expect(wrap.find('h2').text()).toEqual('Its me');
+    expect(wrap.find('h2').get(0).props.style).toHaveProperty(
+      'color',
+      '#FFFFFF'
+    );
+  });
+  it('has image', () => {
+    let wrap = mount<typeof SidebarTitle>(
+      <SidebarTitle title="Its me" image="fakePathToImage" />
+    );
+    expect(wrap.find('img').get(0).props).toHaveProperty(
+      'src',
+      'fakePathToImage'
+    );
+  });
+  it('has background color', () => {
+    let wrap = mount<typeof SidebarTitle>(
+      <SidebarTitle title="Its me" backgroundColor="#FFFFFF" />
+    );
+    expect(wrap.find('div').get(0).props.style).toHaveProperty(
+      'backgroundColor',
+      '#FFFFFF'
+    );
+  });
+  it('has subtitles and color', () => {
+    let wrap = mount<typeof SidebarTitle>(
+      <SidebarTitle
+        title="Its me"
+        subTitle={['subtitle1', 'subtitle2']}
+        subTitleColor="#FFFFFF"
+      />
     );
     expect(wrap.find('h4')).toHaveLength(2);
+    expect(wrap.find('h4').get(0).props.style).toHaveProperty(
+      'color',
+      '#FFFFFF'
+    );
+  });
+  it('has border and color', () => {
+    let wrap = mount<typeof SidebarTitle>(
+      <SidebarTitle
+        title="Its me"
+        bottomBorderSize={1}
+        bottomBorderColor={'#0b4566'}
+      />
+    );
+    expect(wrap.find('div').get(0).props.style).toHaveProperty(
+      'borderBottom',
+      '1px solid #0b4566'
+    );
   });
 });
