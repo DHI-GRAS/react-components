c6733724d7f50bfab79e0ae376e1f581b7b387a3
bertearazvan <bertearazvan@users.noreply.github.com>
2020-09-30 07:33:49 +0000

    Commit latest build

--- dist/components/Legend/Legend.d.ts
+++ dist/components/Legend/Legend.d.ts
@@ -1,8 +1,17 @@
 import * as React from 'react';
 export declare type LegendProps = {
     src: string;
+    /**
+     * *Only relevant when `range` is present. Represents the amount of ticks distributed between min/max values(including them).
+     */
     length?: number | undefined;
+    /**
+     * Min/Max range for the Legend ticks.
+     */
     range?: number[] | string[] | undefined;
+    /**
+     * Append a unit at the end of the values. (%, °C, £, $)
+     */
     unit?: string | undefined;
 };
 declare const Legend: React.FC<LegendProps>;
