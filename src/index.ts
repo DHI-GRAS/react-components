f9dfdeb6eed8a39ba2f369003356ed87beff7b3e
Razvan <rabe@dhigroup.com>
2020-10-27 09:12:30 +0100

    feat(theme): Add Theme to be exported from the library.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { default as AboutCard } from './components/AboutCard';
 import { default as CopyToClipboard } from './components/CopyToClipboard';
 import { default as DashboardCard } from './components/DashboardCard';
 import { default as DynamicForm } from './components/DynamicForm';
+import { default as GrasTheme } from './components/GrasTheme';
 import { default as Information } from './components/Information';
 import { default as Legend } from './components/Legend';
 import { default as Loader } from './components/Loader';
@@ -13,6 +14,7 @@ export {
   CopyToClipboard,
   DashboardCard,
   DynamicForm,
+  GrasTheme,
   Information,
   Legend,
   Loader,
